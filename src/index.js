63fd1c7ec9ae79e210acc31b9f5189557af2318f
Alvaro181529 <aljak1815@gmail.com>
2023-08-09 14:38:10 -0400

    se creo el servido

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-const express = require('express');
+const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
-app.
+app.set("port", 3000);
+
+app.use("morgan");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.listen(app.get("port"), () => {
+  console.log("se habrio el servidor");
+});
